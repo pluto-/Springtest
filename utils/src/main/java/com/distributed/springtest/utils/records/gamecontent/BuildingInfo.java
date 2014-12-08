a5f8c6256297772eccbc1ac25ef9593b52f2f20c
Patrik Nordström <c04pnm@cs.umu.se>
2014-12-08 12:41:07 +0100

    a

--- utils/src/main/java/com/distributed/springtest/utils/records/gamecontent/BuildingInfo.java
+++ utils/src/main/java/com/distributed/springtest/utils/records/gamecontent/BuildingInfo.java
@@ -3,7 +3,7 @@ package com.distributed.springtest.utils.records.gamecontent;
 import com.jajja.jorm.Jorm;
 import com.jajja.jorm.Record;
 
-@Jorm(database= "com/distributed/springtest/utils/records/gamecontent", schema="public", table="buildings", primaryKey="id")
+@Jorm(database= "gamecontent", schema="public", table="buildings", primaryKey="id")
 public class BuildingInfo extends Record {
     public Integer getId() {
         return get("id", Integer.class);
