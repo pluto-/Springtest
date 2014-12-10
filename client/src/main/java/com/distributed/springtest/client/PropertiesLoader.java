8070ec61b8d0fa1e1a744de4669305a0537a936d
c11jmm <c11jmm@cs.umu.se>
2014-12-10 12:39:30 +0100

    Merge branch 'master' of https://github.com/pluto-/Springtest
    Conflicts:
    	utils/src/main/java/com/distributed/springtest/utils/records/playerresources/BuildingInfo.java

---
+++ client/src/main/java/com/distributed/springtest/client/PropertiesLoader.java
@@ -0,0 +1,33 @@
+package com.distributed.springtest.client;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Created by Jonas on 2014-12-08.
+ */
+public class PropertiesLoader {
+
+    public static String getGameContentAddressAndPort() throws IOException {
+        String result = "";
+        Properties prop = new Properties();
+        String propFileName = "client/src/main/resources/admin.properties";
+
+        InputStream inputStream = new FileInputStream(propFileName);
+
+        if (inputStream != null) {
+            prop.load(inputStream);
+        } else {
+            throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+        }
+
+        // get the property value and print it out
+        String address = prop.getProperty("game_content_server_address");
+        String port = prop.getProperty("game_content_server_port");
+
+        return "http://" + address + ":" + port;
+    }
+}
