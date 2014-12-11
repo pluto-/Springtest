3dddd18f6f4618f27cc9e70c74bec16ff1c3c9a9
Patrik Nordström <c04pnm@cs.umu.se>
2014-12-11 15:04:01 +0100

    a

--- client/src/main/java/com/distributed/springtest/client/BuildingsController.java
+++ client/src/main/java/com/distributed/springtest/client/BuildingsController.java
@@ -4,23 +4,19 @@ import com.distributed.springtest.client.forms.BuildingForm;
 import com.distributed.springtest.utils.records.gamecontent.BuildingCost;
 import com.distributed.springtest.utils.records.gamecontent.BuildingInfo;
 import com.distributed.springtest.utils.records.gamecontent.ResourceInfo;
-import com.distributed.springtest.utils.records.playerresources.Building;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.validation.Valid;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Patrik on 2014-12-09.
@@ -54,9 +50,19 @@ public class BuildingsController {
         RestTemplate restTemplate = new RestTemplate();
         BuildingInfo building = restTemplate.getForObject(gamecontentURL + "/buildings/" + id, BuildingInfo.class);
         BuildingCost[] buildingCosts = restTemplate.getForObject(gamecontentURL + "/buildings/" + id + "/costs", BuildingCost[].class);
-        ResourceInfo[] resources = restTemplate.getForObject(gamecontentURL + "/resources", ResourceInfo[].class);
-        modelAndView.addObject("building", building);
-        modelAndView.addObject("buildingCosts", buildingCosts);
+        ResourceInfo[] resourceInfos = restTemplate.getForObject(gamecontentURL + "/resources", ResourceInfo[].class);
+        Map<Integer, String> resources = new HashMap<>();
+        for(ResourceInfo resource : resourceInfos) {
+            resources.put(resource.getId(), resource.getName());
+        }
+        BuildingForm form = new BuildingForm();
+        form.setGeneratedAmount(building.getGeneratedAmount());
+        form.setGeneratedId(building.getGeneratedId());
+        form.setName(building.getName());
+        form.setId(building.getId());
+        form.setBuildtime(building.getBuildtime());
+        form.setBuildingCosts(Arrays.asList(buildingCosts));
+        modelAndView.addObject("form", form);
         modelAndView.addObject("resources", resources);
         modelAndView.addObject("edit", true);
         return modelAndView;
@@ -69,9 +75,22 @@ public class BuildingsController {
         building.setBuildtime(form.getBuildtime());
         building.setGeneratedId(form.getGeneratedId());
         building.setGeneratedAmount(form.getGeneratedAmount());
+        //TODO handle new buildingcosts
         building.save();
         building.transaction().commit();
         return new RedirectView("/buildings");
+    }
+
+    @RequestMapping(value = "/{id}/addCost", method = RequestMethod.POST)
+    public Object addCost(@PathVariable Integer id, @RequestParam Integer newCostResourceId, @RequestParam Integer newCostAmount) {
+        BuildingCost cost = new BuildingCost();
+        cost.setBuildingId(id);
+        cost.setResourceId(newCostResourceId);
+        cost.setAmount(newCostAmount);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(gamecontentURL + "/buildings/" + id + "/costs/add", cost, String.class);
+        System.out.println(responseEntity.getStatusCode() + ": " + responseEntity.getBody());
+        return new RedirectView("/buildings/" + id, true);
     }
 
     @RequestMapping(value = "/new", method = RequestMethod.GET)
