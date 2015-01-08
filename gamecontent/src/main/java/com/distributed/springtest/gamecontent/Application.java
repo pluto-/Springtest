34d37e525d3ca0dd274d80ee4091096eed66044c
c11jmm <c11jmm@cs.umu.se>
2015-01-08 14:54:24 +0100

    Merge branch 'master' of https://github.com/pluto-/Springtest
    Conflicts:
    	auction/src/main/java/com/distributed/springtest/auction/AuctionController.java

--- gamecontent/src/main/java/com/distributed/springtest/gamecontent/Application.java
+++ gamecontent/src/main/java/com/distributed/springtest/gamecontent/Application.java
@@ -7,13 +7,24 @@ package com.distributed.springtest.gamecontent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @ComponentScan
 @EnableAutoConfiguration
-public class Application {
+@EnableWebMvc
+@Configuration
+public class Application extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
 
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new Interceptor());
     }
 }
